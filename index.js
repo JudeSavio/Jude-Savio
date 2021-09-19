d693261168a4ad30b821549a5f10a32687ed3138
Jude_Savio <60323225+JudeSavio@users.noreply.github.com>
2021-09-19 21:24:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function circle(){
+    window.scrollTo(0,0)
+}
+
+window.addEventListener("scroll",function () {
+    info = document.getElementsByClassName("info")[0]
+
+    const rect_info = info.getBoundingClientRect();
+
+    if (
+        rect_info.top >= 0 &&
+        rect_info.left >= 0 &&
+        rect_info.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect_info.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+    {
+        document.getElementsByClassName("circle")[0].style.animation = "none" 
+    }
+    else{
+        console.log('Scroll Event Detected -> Currently in Work Section')
+        document.getElementsByClassName("circle")[0].style.animation = "MoveUpDown 1s linear infinite" 
+    }
+}
+)
